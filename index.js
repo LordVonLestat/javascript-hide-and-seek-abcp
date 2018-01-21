4b5a0cbe0af89e7dd50aba067156634ce336d056
Mike Bakker <equanoxproductions@gmail.com>
2018-01-21 13:40:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector ('#nested .target')
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
+function increaseRankBy(n) {
+  
+}
